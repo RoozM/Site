f3da52bfeb9aa49d3f00c64e17e31f39a7a979b4
Rooz Mahdavian <Rooz@aryas-mac.gateway.2wire.net>
2012-06-30 04:08:32 -0700

    Utility Small Class Changes

--- Crystal/app/Controller/BlogController.php
+++ Crystal/app/Controller/BlogController.php
@@ -6,6 +6,14 @@
 			$blog_data = $this->Blog->Post->find("all");
 			$this->set("posts", $blog_data);
 		}
+		
+		/**
+			This function adds a new blog post. use the auth class.
+		*/
+		public function add(){
+		    $this->_sc();
+		   
+		}
 	
 	}
 ?>
